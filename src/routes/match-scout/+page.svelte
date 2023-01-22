12b6e0ad3aae623cefd0368d882f612c74bc0698
sturmanj <sturmanj@catlin.edu>
2023-01-21 18:44:19 -0800

    Added Siema carousel/page swiping

--- src/routes/match-scout/+page.svelte
+++ src/routes/match-scout/+page.svelte
@@ -8,18 +8,43 @@
   import EndNotes from "../../lib/components/EndNotes.svelte";
   import AutoScore from "../../lib/components/AutoScore.svelte";
   import TeleScore from "../../lib/components/TeleScore.svelte";
+  import Siema from "siema";
+  import { onMount } from "svelte";
+  
+  onMount(() => {
+    new Siema({
+      selector: ".carousel",
+      duration: 200,
+      easing: "ease-in-out",
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: false,
+      threshold: 20,
+      loop: false,
+      rtl: false,
+      onInit: () => {},
+      onChange: () => {},
+    });
+  })
 </script>
 
 <h1 class="text-red-600 text-4xl text-center font-bold">Match Scout</h1>
 
-<AutoScore />
-<AutoChargeStation />
-<AutoCommunity />
-
-<TeleScore />
-
-<EndChargeStation />
-<EndDriverSkill />
-<EndBroke />
-<EndDied />
-<EndNotes />
+<div class="carousel">
+  <div id="auto">
+    <AutoScore />
+    <AutoChargeStation />
+    <AutoCommunity />
+  </div>
+  <div id="tele">
+    <TeleScore />
+  </div>
+  <div id="end">
+    <EndChargeStation />
+    <EndDriverSkill />
+    <EndBroke />
+    <EndDied />
+    <EndNotes />
+  </div>
+</div>
