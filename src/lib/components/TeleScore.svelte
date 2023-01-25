16cb41fc796c9e2dcc64ad654f3a53c6fa933134
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-01-25 15:17:59 -0800

    Example for Henry

--- src/lib/components/TeleScore.svelte
+++ src/lib/components/TeleScore.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
     import { tele_score } from "$lib/matchScoutStores";
+	let clicked: any;
+    let miliSecondsArr: number[];
+    let miliSeconds: number;
+
+    function handleMousedown() {
+        console.log("test");
+        clicked = true;
+        miliSeconds = 0;
+        miliSeconds += 1;
+        while ( clicked ) { 
+            setInterval(() => miliSeconds += 1, 1);
+            console.log(miliSeconds);
+        }
+        clearInterval;
+
+        miliSecondsArr.push(miliSeconds);
+        console.log(miliSeconds);
+	}
+
+    function handleMouseup() {
+        clicked = false;
+    }
 </script>
 
 <h1 class="text-red-600 text-center">TeleScore</h1>
+
+<button on:touchstart={handleMousedown} on:touchend={handleMouseup} on:click={handleMousedown}>Test</button>
+
+<style>
+</style>
