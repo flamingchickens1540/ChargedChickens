b6ebbb8e34f7ec20693b637b47e1ea83131e365f
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-01-27 12:45:26 -0800

    Added EndChargeStation options

--- src/lib/components/EndChargeStation.svelte
+++ src/lib/components/EndChargeStation.svelte
@@ -5,4 +5,7 @@
 </script>
 
 
-<h1 class="text-red-600 text-center">EndChargeStation</h1>
+<h1 class="text-red-600 text-center"><input type="checkbox"/>Not Attempted</h1>
+<h1 class="text-red-600 text-center"><input type="checkbox"/>Failed</h1>
+<h1 class="text-red-600 text-center"><input type="checkbox"/>Docked</h1>
+<h1 class="text-red-600 text-center"><input type="checkbox"/>Engaged</h1>
