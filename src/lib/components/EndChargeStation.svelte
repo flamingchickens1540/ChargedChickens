0ced74aaaea9d8f42bcb4c97412e6521d1082543
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-01-27 08:17:11 -0800

    Added partial EndBroke input functionality

--- src/lib/components/EndChargeStation.svelte
+++ src/lib/components/EndChargeStation.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import { end_charge_station } from "$lib/matchScoutStores";
+
+
 </script>
+
 
 <h1 class="text-red-600 text-center">EndChargeStation</h1>
--- src/lib/components/EndDied.svelte
+++ src/lib/components/EndDied.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { died } from "$lib/matchScoutStores";
+    
+    
 </script>
 
 <h1 class="text-blue-500 text-center">EndDied</h1>
