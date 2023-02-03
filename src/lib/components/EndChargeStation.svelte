0ac4cf9f4d7a3644226f603c51d17212eb706e4e
DaviTheDino <119637962+DaviTheDinosaur@users.noreply.github.com>
2023-02-03 09:11:16 -0800

    EndDriverSkill Stars and End Charge Station Components Done

--- src/lib/components/EndChargeStation.svelte
+++ src/lib/components/EndChargeStation.svelte
@@ -1,5 +1,71 @@
 <script lang="ts">
     import { end_charge_station } from "$lib/matchScoutStores";
+    let promisingOptionOneOfTheManyOnes = "Current Selected: Not Attempted";
 </script>
 
 <h1 class="text-red-600 text-center">EndChargeStation</h1>
+<div class="centerButtons">
+    <div class="content-center">
+        <div>
+            <label>
+                <input
+                    type="radio"
+                    bind:group={promisingOptionOneOfTheManyOnes}
+                    value={"Current Selected: Not Attempted"}
+                />
+                Not Attempted
+            </label>
+        </div>
+        <div>
+            <label>
+                <input
+                    type="radio"
+                    bind:group={promisingOptionOneOfTheManyOnes}
+                    value={"Current Selected: Failed"}
+                />
+                Failed
+            </label>
+        </div>
+        <div>
+            <label>
+                <input
+                    type="radio"
+                    bind:group={promisingOptionOneOfTheManyOnes}
+                    value={"Current Selected: Docked"}
+                />
+                Docked
+            </label>
+        </div>
+        <div>
+            <label>
+                <input
+                    type="radio"
+                    bind:group={promisingOptionOneOfTheManyOnes}
+                    value={"Current Selected: Engaged"}
+                />
+                Engaged
+            </label>
+        </div>
+    </div>
+
+</div>
+
+<div class = "centerButtonsValue">
+    <h1>
+        {promisingOptionOneOfTheManyOnes}
+    </h1>
+</div>
+
+
+<style>
+    .centerButtons {
+        display: flex;
+        justify-content: center;  
+        
+    }
+
+    .centerButtonsValue{
+        display: flex;
+        justify-content: center;
+    }
+</style>
--- src/lib/components/EndDriverSkill.svelte
+++ src/lib/components/EndDriverSkill.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
+    import { applyAction } from "$app/forms";
     import { skill } from "$lib/matchScoutStores";
+    import { Rating } from "svelte-stars-hover-rating";
 </script>
 
 <h1 class="text-orange-400 text-center">EndDriverSkill</h1>
+
+<div class = "centerStars">
+    <Rating bind:rating={$skill} />
+</div>
+
+<style>
+
+    .centerStars{
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+        rotate: -90deg;
+    }
+
+</style>
