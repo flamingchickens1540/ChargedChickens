8167744e87051090f481b6e5683629d11e06eafa
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-04 11:58:33 -0800

    Cleaned up TeleScore

--- src/lib/components/TeleScore.svelte
+++ src/lib/components/TeleScore.svelte
@@ -12,30 +12,10 @@
       miliSecondsArr.push(Date.now() - initialTime)
     }
 
-    import { Canvas, Layer, t } from 'svelte-canvas';
+    import { Canvas, Layer } from 'svelte-canvas';
 
-    // // Grid width
-    // var bw = 120;
-    // // Grid height
-    // var bh = 120;
-    // // Padding
-    // var p = 10;
       // @ts-ignore
     $: render = ({ context, width, height }) => {
-      // The number of boxes is determined by the ratio between the incrementer of x, and the base/height of the grid
-
-      // for (let y = 0; y <= bw; y += 40) {
-      //   context.moveTo(0.5 + y + p, p);
-      //   context.lineTo(0.5 + y + p, bh + p);
-      // }
-    
-      // for (let x = 0; x <= bh; x += 40) {
-      //     context.moveTo(p, 0.5 + x + p);
-      //     context.lineTo(bw + p, 0.5 + x + p);
-      // }
-      // context.strokeStyle = "black";
-      // context.stroke();
-
       const TeleOpScoreBoard = new Image();
 
       TeleOpScoreBoard.src = "../assets/TeleOpScoring.png";
@@ -44,7 +24,7 @@
       };
     };
     
-    //Jack code
+    // TODO: Jack code to be implemented on canvas
     // function mouseClicked() {
     //   if (mouseX < 0 || mouseX > 900 || mouseY < 0 || mouseY > 300) return;
     //   row = Math.floor(mouseY / 100);
