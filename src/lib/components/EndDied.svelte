3186b0c4aa37f4b0f82155c103e9d8c7724bef03
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-04 10:57:32 -0800

    fixed: EndDied function

--- src/lib/components/EndDied.svelte
+++ src/lib/components/EndDied.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
     import { died } from "$lib/matchScoutStores";
 
-
-</script>
-
-<h1 class="text-blue-500 text-center">EndDied</h1>
-    
     function toggleDied(die : boolean) {
         died.set(die);
     }
 </script>
 
+<h1 class="text-blue-500 text-center">EndDied</h1>
+    
+    
 <center>
     <h1 class="text-blue-500 text-center">EndDied</h1>
     <button on:touchend={() => toggleDied(true)}>Yes</button>
