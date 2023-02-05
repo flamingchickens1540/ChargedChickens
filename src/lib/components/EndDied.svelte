02a4cbc8832ef0ca2add9b2ccb7a8faa6a1d1be3
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-04 17:06:14 -0800

    EndDriverSkill Stars and End Charge Station Components Done (#5)
    * EndDriverSkill Stars and End Charge Station Components Done
    
    * fixed: EndDied function
    
    * cleanup: renamed variable in EndChangeStation for more clarity
    
    ---------
    
    Co-authored-by: DaviTheDino <119637962+DaviTheDinosaur@users.noreply.github.com>

--- src/lib/components/EndDied.svelte
+++ src/lib/components/EndDied.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import { died } from "$lib/matchScoutStores";
-    
+
     function toggleDied(die : boolean) {
         died.set(die);
     }
 </script>
 
+<h1 class="text-blue-500 text-center">EndDied</h1>
+    
+    
 <center>
     <h1 class="text-blue-500 text-center">EndDied</h1>
     <button on:touchend={() => toggleDied(true)}>Yes</button>
