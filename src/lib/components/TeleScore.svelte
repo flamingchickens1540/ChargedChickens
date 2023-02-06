18601eb0bca74786060e5fbdc5056d512a3578b1
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-06 15:52:58 -0800

    TeleScore grid improvements

--- src/lib/components/TeleScore.svelte
+++ src/lib/components/TeleScore.svelte
@@ -1,40 +1,111 @@
 <script lang="ts">
-    import TeleOpScoring from "$lib/assets/TeleOpScoring.png"
-    import { tele_score } from "../matchScoutStores";
+  import TeleOpScoring from "$lib/assets/TeleOpScoring.png";
+  import { tele_score } from "../matchScoutStores";
 
-    let innerHeight = 0;
-    let innerWidth = 0;
+  import { Canvas, Layer } from "svelte-canvas";
+  import DefenceButton from "./DefenceButton.svelte";
 
-    import { Canvas, Layer } from 'svelte-canvas';
-    import DefenceButton from "./DefenceButton.svelte";
+  let innerHeight = 0;
+  let innerWidth = 0;
+  let elementHeight = 0;
 
-      // @ts-ignore
-    $: render = ({ context, width, height }) => {
-      const TeleOpScoreBoard = new Image();
+  let mouse = { x: 0, y: 0 };
+  // @ts-ignore
+  $: render = ({ context, width, height }) => {
+    const TeleOpScoreBoard = new Image();
 
-      TeleOpScoreBoard.src = TeleOpScoring;
-      TeleOpScoreBoard.onload = () => {
-        context.drawImage(TeleOpScoreBoard, 0, 0, width, height);
-      };
+    TeleOpScoreBoard.src = TeleOpScoring;
+    TeleOpScoreBoard.onload = () => {
+      context.drawImage(TeleOpScoreBoard, 0, 0, width, height);
     };
+  };
+  // TODO: Jack code to be implemented on canvas
+  function mouseClicked() {
+    if (mouse.x < 0 || mouse.x > innerWidth || mouse.y < 0 || mouse.y > elementHeight)
+      return;
+    let row = Math.floor(mouse.y / elementHeight);
+    let col = Math.floor(mouse.x / innerWidth);
     
-    // TODO: Jack code to be implemented on canvas
-    // function mouseClicked() {
-    //   if (mouseX < 0 || mouseX > 900 || mouseY < 0 || mouseY > 300) return;
-    //   row = Math.floor(mouseY / 100);
-    //   col = Math.floor(mouseX / 100);
-    //   if (socket != null) socket.emit("click", { row, col });
-    // }
+    if (col == 1) {
+      if (row == 1) {
+        tele_score.tele_high_left_succeed.update(n => n++);
+      } else if (row == 2) {
+        tele_score.tele_mid_left_succeed.update(n => n++);
+      } else if (row == 3) {
+        tele_score.tele_low_left_succeed.update(n => n++);
+      } 
+    } else if (col == 2) {
+      if (row == 1) {
+        tele_score.tele_high_center_succeed.update(n => n++);
+      } else if (row == 2) {
+        tele_score.tele_mid_center_succeed.update(n => n++);
+      } else if (row == 3) {
+        tele_score.tele_low_center_succeed.update(n => n++);
+      }
+    } else if (col == 2) {
+      if (row == 1) {
+        tele_score.tele_high_right_succeed.update(n => n++);
+      } else if (row == 2) {
+        tele_score.tele_mid_right_succeed.update(n => n++);
+      } else if (row == 3) {
+        tele_score.tele_low_right_succeed.update(n => n++);
+      }
+    } 
+    console.log("clicked");
+    console.log(tele_score);
+  }
+
+  function mouseDoubleClicked() {
+    if (mouse.x < 0 || mouse.x > innerWidth || mouse.y < 0 || mouse.y > elementHeight)
+      return;
+    let row = Math.floor(mouse.y / innerHeight);
+    let col = Math.floor(mouse.x / innerWidth);
+    
+    if (col == 1) {
+      if (row == 1) {
+        tele_score.tele_high_left_fail.update(n => n++);
+      } else if (row == 2) {
+        tele_score.tele_mid_left_fail.update(n => n++);
+      } else if (row == 3) {
+        tele_score.tele_low_left_fail.update(n => n++);
+      } 
+    } else if (col == 2) {
+      if (row == 1) {
+        tele_score.tele_high_center_fail.update(n => n++);
+      } else if (row == 2) {
+        tele_score.tele_mid_center_fail.update(n => n++);
+      } else if (row == 3) {
+        tele_score.tele_low_center_fail.update(n => n++);
+      }
+    } else if (col == 2) {
+      if (row == 1) {
+        tele_score.tele_high_right_fail.update(n => n++);
+      } else if (row == 2) {
+        tele_score.tele_mid_right_fail.update(n => n++);
+      } else if (row == 3) {
+        tele_score.tele_low_right_fail.update(n => n++);
+      }
+    }
+    console.log("clicked");
+    console.log(tele_score);
+  }
+  
 </script>
 
 <svelte:window bind:innerHeight bind:innerWidth />
 
 <h1 class="text-red-600 text-center">TeleScore</h1>
 
-<Canvas width={innerWidth} height={450} class="object-center">
+<Canvas
+  width={innerWidth}
+  height={450}
+  class="object-center"
+  on:click={mouseClicked} on:dblclick={mouseDoubleClicked}
+>
   <Layer {render} />
 </Canvas>
 
-<DefenceButton></DefenceButton>
+<DefenceButton />
+
 <style>
 </style>
