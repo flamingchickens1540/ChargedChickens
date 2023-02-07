06b04a6cb108869673d93c23bbb630adf2bb5966
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-06 16:02:50 -0800

    Addded DefenceButton component(forgot git add. the first time)

---
+++ src/lib/components/DefenceButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { defense_times } from "$lib/matchScoutStores";
+
+    let miliSecondsArr: number[] = [];
+    let initialTime : number;
+
+    function handleMousedown() {
+        initialTime = Date.now();
+	}
+    function handleMouseup() {
+      miliSecondsArr.push(Date.now() - initialTime)
+      defense_times.update(() => miliSecondsArr);
+      console.log($defense_times);
+    }
+</script>
+
+<button on:touchstart={handleMousedown} on:touchend={handleMouseup} on:mousedown={handleMousedown} on:mouseup={handleMouseup} class="border">Defense</button>
