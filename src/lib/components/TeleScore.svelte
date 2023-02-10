a8a8a2cca8863bb3897ae709dfc1305ace82fca2
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-09 23:59:46 -0800

    feat: Changed teleopscoring image to fit color scheme and removed Match Score header

--- src/lib/components/TeleScore.svelte
+++ src/lib/components/TeleScore.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import TeleOpScoring from "$lib/assets/TeleOpScoring.png";
+  import TeleOpScoring from "$lib/assets/Teleop.png";
   import { tele_high_center_fail, tele_high_center_succeed, tele_high_left_fail, tele_high_left_succeed, tele_high_right_fail, tele_high_right_succeed, tele_low_center_fail, tele_low_center_succeed, tele_low_left_fail, tele_low_left_succeed, tele_low_right_fail, tele_low_right_succeed, tele_mid_center_fail, tele_mid_center_succeed, tele_mid_left_fail, tele_mid_left_succeed, tele_mid_right_fail, tele_mid_right_succeed, tele_score } from "../matchScoutStores";
   import { DEBUG } from "../generalStores";
 
--- src/routes/match-scout/+page.svelte
+++ src/routes/match-scout/+page.svelte
@@ -33,7 +33,7 @@
 
 <body>
   {#if $login || $admin}
-  <h1 class="text-red-600 text-4xl text-center font-bold">Match Scout</h1>  
+  <!-- <h1 class="text-red-600 text-4xl text-center font-bold">Match Scout</h1>   -->
   <div id="carousel" class="h-screen w-full">
     <div id="auto">
       <AutoScore />
