f85e62ae80dbd8c52cd54ba2e102fc83f9f2159d
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-02-13 10:50:55 -0800

    Revert "RatingFix"
    This reverts commit e818f6f6dcc63c392f380dc437397e542d044065.

--- src/lib/components/Rating.svelte
+++ src/lib/components/Rating.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import Star from "./Star.svelte";
 
-	export let rating = 3;
+	export let rating = 0;
 	export let num = 5;
 	let hoverRating: number | null = null;
-	const handleRate = (id: number) => () => rating = id;
-	let stars = Array.from({ length: num }, (__, i) => i);
+	const handleRate = (id: number) => () => {
+		if (rating == 1 && id == 1) {
+			rating = 0;
+		} else {
+			rating = id;
+		}
+	};
+	let stars = Array.from({ length: num }, (__, i) => i + 1);
 </script>
 
 <div class="feedback">
@@ -15,6 +21,7 @@
 				<Star
 					filled={hoverRating ? hoverRating >= star : rating >= star}
 					starValue={star}
+					on:mouseleave={() => (hoverRating = null)}
 					on:click={handleRate(star)}
 				/>
 			{/each}
