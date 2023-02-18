a4f8b039da5c028bab2eadb32c8746156475d639
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-17 18:19:40 -0800

    feat: Added login and admin-dashboard

--- src/lib/stores/generalStores.ts
+++ src/lib/stores/generalStores.ts
@@ -3,6 +3,8 @@ import type { MatchScoutInfo } from "$lib/types";
 
 export const DEBUG: Writable<boolean> = writable(false);
 
+export const user: Writable<string> = writable("Anon");
+
 export const info: Writable<MatchScoutInfo> = writable(
   {
     success: false,
