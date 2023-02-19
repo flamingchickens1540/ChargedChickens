70422b25aea6e8ba12caa0b216b57871c19c3bb1
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-02-18 18:22:50 -0800

    fix

--- src/routes/api/assign/+server.ts
+++ src/routes/api/assign/+server.ts
@@ -3,16 +3,16 @@ import { setRobots } from "$lib/server-assets/scoutQueue";
 import type { AssignData, Robot } from "$lib/types";
 import type { RequestEvent } from "./$types";
 import { DEBUG } from "$lib/generalStores";
-import type  { TeamKey } from "$lib/types";
 
 export const POST = async ({request}: RequestEvent) => {
   const data: AssignData = await request.json()
 
+  if(DEBUG) console.log(data);
+  if(!data.robots) return json({"success": false, "endpoint": "assign"});
+
   const robots: Robot[] = [];
 
   data.robots.red.forEach(teamKey => {
-    if (DEBUG) console.log(data.robots.red);
-    // TODO: Fix TypeError: Cannot read properties of undefined (reading 'red')
     robots.push({
       "team_key": teamKey,
       "alliance": "red"
