5c1c1ac9548577e41d4fa419e3b3c586c593a031
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-02-25 00:46:07 -0800

    feat: :sparkles: For pitscouting, added Dimensions, Drivetrain, and Intake

--- src/lib/components/pit-scout-components/Drivetrain.svelte
+++ src/lib/components/pit-scout-components/Drivetrain.svelte
@@ -1,5 +1,122 @@
 <script lang="ts">
   import { drivetrain } from "$lib/stores/pitScoutStores";
+
+  var backgroundColorsButtons = ["#efdcdc","#efdcdc","#efdcdc"]
+
+function handleSwerve(){
+      drivetrain.set(0);
+      for (let i = 0; i < 3; i++){
+          if (i == 0){
+              backgroundColorsButtons[i] = "#7ED957";
+          }else{
+              backgroundColorsButtons[i] = "#efdcdc"; 
+          }
+      }
+  }
+  function handleTank(){
+      drivetrain.set(1);
+      for (let i = 0; i < 3; i++){
+          if (i == 1){
+              backgroundColorsButtons[i] = "#7ED957";
+          }else{
+              backgroundColorsButtons[i] = "#efdcdc"; 
+          }
+      }
+  }
+  function handleOther(){
+      drivetrain.set(2);
+      for (let i = 0; i < 3; i++){
+          if (i == 2){
+              backgroundColorsButtons[i] = "#7ED957";
+          }else{
+              backgroundColorsButtons[i] = "#efdcdc"; 
+          }
+      }
+  }
 </script>
 
-<h1 class="text-orange-600 text-xl text-center">Drivetrain</h1>
+<h1 class="text-yellow-500 text-xl text-center">Drivetrain</h1>
+
+<div class="grid place-items-center">
+<div class = "makeColumnButtons">
+  <div class = "buttonsTitle"> 
+      Drivetrain Type
+  </div>
+  <button class = "optionButtons" on:click = {handleSwerve} style = "background-color: {backgroundColorsButtons[0]}"> Swerve </button>
+  <button class = "optionButtons" on:click = {handleTank} style = "background-color: {backgroundColorsButtons[1]}"> Tank </button>
+  <button class = "optionSpecialButtons" on:click = {handleOther} style = "background-color: {backgroundColorsButtons[2]}"> Other </button>
+</div>
+</div>
+
+<style>
+
+.makeColumnButtons{
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+}
+
+.buttonsTitle{
+    text-align: center;
+    font-weight: 700;
+    font-family: "Poppins";
+    font-size: 23px;
+    border-bottom-width: 2px;
+    height: 51px;
+    border-color: black;
+    padding-top: 6px;
+    background-color: #efdcdc;
+    border-width: 2px;
+    border-top-right-radius: 0.3rem;
+    border-top-left-radius: 0.3rem;
+}
+
+.endgameTitle{
+    display: flex;
+    font-family: "Poppins";
+    font-size: 36px; 
+    padding-top: 11px;
+    padding-left: 2px;      
+    width: 50%;
+}
+
+.alignTitle{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.endgameTitleNumbers{
+    display: flex;
+    font-family: "Poppins";
+    font-size: 36px; 
+    padding-top: 11px;
+    width: 50%;
+    justify-content: right;
+    padding-right: 11px;
+}
+.optionButtons{
+    height: 47px;
+    border-bottom-width: 2px;
+    border-color: black;
+    text-align: left;
+    text-indent: 10px;
+    font-family: "Poppins";
+    font-size: 23px;
+    border-left-width: 2px;
+    border-right-width: 2px;
+}
+.optionSpecialButtons{
+    height: 47px; 
+    text-align: left;
+    text-indent: 10px;
+    font-family: "Poppins";
+    font-size: 23px;
+    border-bottom-right-radius: 0.3rem;
+    border-bottom-left-radius: 0.3rem;
+    border-left-width: 2px;
+    border-right-width: 2px;
+    border-bottom-width: 2px;
+    border-color: black;
+}
+</style>
