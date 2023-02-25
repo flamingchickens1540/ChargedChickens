6d646adcbae66098970a6ab5b9395bf67ac44641
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-02-25 15:30:18 -0800

    Revert "Update src/lib/components/match-scout-components/AutoScore.svelte"
    This reverts commit 01f6a812b4a64bce292f61106990771c9bf440b2.

--- src/lib/components/match-scout-components/AutoScore.svelte
+++ src/lib/components/match-scout-components/AutoScore.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { auto_high_center_fail, auto_high_center_succeed, auto_high_left_fail, auto_high_left_succeed, auto_high_right_fail, auto_high_right_succeed, auto_low_center_fail, auto_low_center_succeed, auto_low_left_fail, auto_low_left_succeed, auto_low_right_fail, auto_low_right_succeed, auto_mid_center_fail, auto_mid_center_succeed, auto_mid_left_fail, auto_mid_left_succeed, auto_mid_right_fail, auto_mid_right_succeed, auto_score } from "$lib/stores/matchScoutStores";
-    import ScoreTable from "$lib/components/ui-components/ScoreTable.svelte";
+    import ScoreTable from "../ui-components/ScoreTable.svelte";
 
     export const autoScoreSucceed = [
         auto_high_left_succeed, 
