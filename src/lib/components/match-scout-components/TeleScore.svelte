e229622a3c675c2d4e7b6eb768820a070a6bcff5
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-02-25 15:30:06 -0800

    Revert "Update src/lib/components/match-scout-components/TeleScore.svelte"
    This reverts commit cc3d52b3f176d889e6d39ac4c0dcab6b34cdfaa0.

--- src/lib/components/match-scout-components/TeleScore.svelte
+++ src/lib/components/match-scout-components/TeleScore.svelte
@@ -2,7 +2,7 @@
   import { tele_high_center_fail, tele_high_center_succeed, tele_high_left_fail, tele_high_left_succeed, tele_high_right_fail, tele_high_right_succeed, tele_low_center_fail, tele_low_center_succeed, tele_low_left_fail, tele_low_left_succeed, tele_low_right_fail, tele_low_right_succeed, tele_mid_center_fail, tele_mid_center_succeed, tele_mid_left_fail, tele_mid_left_succeed, tele_mid_right_fail, tele_mid_right_succeed, tele_score } from "$lib/stores/matchScoutStores";
   // import { DEBUG } from "../generalStores";
   import DefenseButton from "$lib/components/match-scout-components/DefenseButton.svelte";
-  import ScoreTable from "$lib/components/ui-components/ScoreTable.svelte";
+  import ScoreTable from "../ui-components/ScoreTable.svelte";
 
   const teleScoreSucceed = [
     tele_high_left_succeed, 
