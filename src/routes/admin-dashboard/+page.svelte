f479c298f75e587d60b3fbf6ffdbb56fe84c430f
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-03-08 23:26:08 -0800

    feat: re-arranged grid on admin-dash

--- src/routes/admin-dashboard/+page.svelte
+++ src/routes/admin-dashboard/+page.svelte
@@ -132,11 +132,11 @@
 <div class="grid grid-rows-1 grid-cols-2 text-center">
 
     <div class="p-4 col-span-2 grid grid-rows-1 grid-cols-1 outline">
-        <h1 class="text-center text-3xl text-purple-600"><strong> Welcome Admin </strong></h1>
+        <h1 class="text-center text-3xl text-blue-600"><strong> Welcome Admin </strong></h1>
     </div>
 
     <div class="p-10 grid grid-cols-1 grid-rows-2 gap-5 place-self-center outline">
-        <h1 class="text-blue-500"><strong> Blue Robots </strong></h1>
+        <h1 class="text-blue-600"><strong> Blue Robots </strong></h1>
         <input type="text" bind:value={robots_blue[0]}>
         <input type="text" bind:value={robots_blue[1]}>
         <input type="text" bind:value={robots_blue[2]}>
@@ -151,23 +151,33 @@
 </div>
 
 <div class="grid grid-rows-1 grid-cols-2 text-center">
-    <div class="p-10 grid grid-cols-1 grid-rows-2 gap-5 place-items-center outline">
-        <label class="text-red-500" for="match_key"><strong> Match Key </strong></label>
+
+    <div class="p-4 grid grid-cols-1 grid-rows-2 gap-5 place-items-center outline">
+        <label class="text-blue-600" for="match_key"><strong> Match Key </strong></label>
         <input type="text" name="match_key" bind:value={match_key}>
     </div>
-    <div class="grid grid-cols-1 grid-rows-1 place-items-center outline">
-        <button class="h-32 w-32 lg:flex-grow sm:flex-shrink rounded-full outline" on:click={createMatch}> Create Match </button>
+
+    <div class="h-24 grid grid-cols-1 grid-rows-1 place-items-center outline ">
+        <button class="h-14 lg:flex-grow sm:flex-shrink text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={autoPopulate}> Auto-Populate </button>
     </div>
-    <div class="h-36 grid grid-cols-1 grid-rows-2 place-items-center outline">
-        <input type="text" bind:value={inputPassword}>
-        <button class="h-18 text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={() => setPassword()}> Auth </button>
+
+    <div class="grid grid-cols-1 grid-rows-2 place-items-center outline">
+        <strong><p class="text-blue-600">Last Created Match</p></strong>
+        <strong><p class="text-blue-600">{lastCreatedMatch}</p></strong>
     </div>
+
+    <div class="h-24 grid grid-cols-1 grid-rows-1 place-items-center outline">
+        <button class="h-14 text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={createMatch}> Create Match </button>
+    </div>
+    
     <div class="h-36 grid grid-cols-1 grid-rows-2 place-items-center outline">
         <input type="text" bind:value={event_key}>
-        <button class="h-18 text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={() => createEvent()}> Create Event </button>
+        <button class="h-14 text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={() => createEvent()}> Create Event </button>
     </div>
-    <div class="h-24 grid grid-cols-1 grid-rows-1 place-items-center outline col-span-2">
-        <button class="lg:flex-grow sm:flex-shrink text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={autoPopulate}> Auto-Populate </button>
+
+    <div class="h-36 grid grid-cols-1 grid-rows-2 place-items-center outline">
+        <input type="text" bind:value={inputPassword}>
+        <button class="h-14 text-red-600 text-lg p-4 rounded bg-yellow-300 outline" on:click={() => setPassword()}> Auth </button>
     </div>
 
 </div>
