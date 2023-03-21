bcf91cec0d534ffe2d1eed6db649f2b7ed269bbf
Elo Colburn <62953415+Ewie21@users.noreply.github.com>
2023-03-21 16:49:30 -0700

    Update EndNotes.svelte

--- src/lib/components/EndNotes.svelte
+++ src/lib/components/EndNotes.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
-    import { notes } from "$lib/matchScoutStores";
+    import { notes } from '$lib/stores/matchScoutStores'
 </script>
 
-<h1 class="text-red-600 text-center">EndNotes</h1>
+<div>
+    <div class="makeBorder">
+        Extra Notes
+        <textarea bind:value={$notes} class="notesBox" placeholder="Specific Gracious Professional Comments" />
+    </div>
+</div>
+
+<style>
+    .notesBox {
+        width: 100%;
+        resize: none;
+        height: 70px;
+    }
+    .makeBorder {
+        font-family: 'Poppins';
+        flex-direction: column;
+        background-color: #efdcdc;
+        border-color: black;
+        font-size: 23px;
+        border-width: 2px;
+        border-radius: 0.5rem;
+        justify-content: center;
+        padding: 11px;
+    }
+</style>
