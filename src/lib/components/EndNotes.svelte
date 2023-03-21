046729ea5356db77e2a5131e6c7b56148e8be17b
keveenthebean <124744491+keveenthebean@users.noreply.github.com>
2023-03-21 16:50:15 -0700

    feat: removed david button (#73)
    * feat: removed david button
    
    * feat: Removed David Button
    
    * Update EndNotes.svelte
    
    ---------
    
    Co-authored-by: Elo Colburn <62953415+Ewie21@users.noreply.github.com>

---
+++ src/lib/components/EndNotes.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { notes } from '$lib/stores/matchScoutStores'
+</script>
+
+<div>
+    <div class="makeBorder">
+        Extra Notes
+        <textarea bind:value={$notes} class="notesBox" placeholder="Specific Gracious Professional Comments" />
+    </div>
+</div>
+
+<style>
+    .notesBox {
+        width: 100%;
+        resize: none;
+        height: 70px;
+    }
+    .makeBorder {
+        font-family: 'Poppins';
+        flex-direction: column;
+        background-color: #efdcdc;
+        border-color: black;
+        font-size: 23px;
+        border-width: 2px;
+        border-radius: 0.5rem;
+        justify-content: center;
+        padding: 11px;
+    }
+</style>
